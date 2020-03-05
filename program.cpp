6922d6e2cda363fa77cc7bfebd1c14c04bd72b26
yulia minina <yulia-pt@yandex.ru>
2020-03-05 13:59:16 +0300

    Snapshot 1, program.cpp

---
+++ program.cpp
@@ -0,0 +1,13 @@
+//файл program.cpp
+#include "stdafx.h"
+#include <iostream>
+#include "funcs.h"
+using namespace std; 
+ 
+
+int main() 
+{  
+	cout << MySum(10, 3) << endl; 
+	cout << MySub(10, 3) << endl;    
+	return 0; 
+ } 
